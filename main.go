90a46c24c08bef6f362f159eaa34937a245d143f
pallat <yod.pallat@gmail.com>
2017-05-10 22:34:13 +0700

    todo list api only get and create

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+var db *gorm.DB
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+type todo struct {
+	gorm.Model
+	Topic string `json:"topic"`
+	Done  bool   `json:"done"`
+}
+
+func getTodos(c echo.Context) error {
+	var todos []todo
+	db = db.Find(&todos)
+	if db.Error != nil {
+		return db.Error
+	}
+
+	return c.JSON(http.StatusOK, todos)
+}
+
+func newTodo(c echo.Context) error {
+	var t todo
+
+	err := c.Bind(&t)
+	if err != nil {
+		return err
+	}
+
+	db = db.Create(&t)
+	if db.Error != nil {
+		return db.Error
+	}
+
+	return c.JSON(http.StatusOK, t)
+}
+
+func init() {
+	var err error
+	db, err = gorm.Open("sqlite3", "local.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(todo{})
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+	e.GET("/todos", getTodos)
+	e.POST("/todo", newTodo)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
